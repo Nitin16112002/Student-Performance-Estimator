ac5c4e085a29dea27187579c636068127e8592d8
Nitin <bcs.animesh@gmail.com>
2023-07-30 18:42:34 +0530

    Loggers and Exception

--- setup.py
+++ setup.py
@@ -4,31 +4,31 @@ from pip.req.req_file import parse_requirements
 
 hyphen_e_dot="-e ."
 
-# def get_requirements(file_path:str)->List[str]:
-#     # This function returns the list of requirements
-#     requirements=[]
-#     with open (file_path) as file_obj:
-#         requirements=file_obj.readlines()
-#         requirements=[req.replace('\n','') for req in requirements] # so that '\n' is ignored while reading requirements.txt    
-#         # This is ListComprehension
+def get_requirements(file_path:str)->List[str]:
+    # This function returns the list of requirements
+    requirements=[]
+    with open (file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements] # so that '\n' is ignored while reading requirements.txt    
+        # This is ListComprehension
 
-#         if hyphen_e_dot in requirements:
-#             requirements.remove(hyphen_e_dot) # so that '-e .' is ignored while reading requirements.txt
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot) # so that '-e .' is ignored while reading requirements.txt
 
-#     return requirements
-
-
-
-
-
-def get_requirements(file_path: str) -> List[str]:
-    requirements = []
-    
-    for req in parse_requirements(file_path, session=""):
-        if not req.requirement.startswith("-e"):
-            requirements.append(str(req.requirement))
-    
     return requirements
+
+
+
+
+
+# def get_requirements(file_path: str) -> List[str]:
+#     requirements = []
+    
+#     for req in parse_requirements(file_path, session=""):
+#         if not req.requirement.startswith("-e"):
+#             requirements.append(str(req.requirement))
+    
+#     return requirements
 
 
 
@@ -46,3 +46,7 @@ setup(
 
 
 )
+
+
+# __init__.py is used to convert a directory into a library so 
+# that files there could be automatically imported and used
